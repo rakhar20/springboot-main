f5eeab89b667411fa526a1e9ad13688f902d2bc4
rakhar20 <prakharjoshi25@gmail.com>
2024-04-01 17:00:03 +0530

    1

---
+++ src/main/java/com/example/demo/helloContainer.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
+
+public class helloContainer {
+
+    @GetMapping("/")
+	public Object hello(){
+		Map<String, String> object = new HashMap<>();
+		object.put("name", "prakharjoshi25125");
+		object.put("email", "prakharjoshi25@gmail.com");
+		return object;
+	}
+	public String index() {
+		return "First Spring Boot Project!";
+	}
+}
